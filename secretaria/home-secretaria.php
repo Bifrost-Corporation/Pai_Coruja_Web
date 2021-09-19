52db6052a2fa7890e507844f6f5cf8e966d9d9a0
Douglas Nascimento <douglas.dans@gmail.com>
2021-09-19 17:30:37 -0300

    Menu novo adicionado em tds as páginas

--- secretaria/home-secretaria.php
+++ secretaria/home-secretaria.php
@@ -23,127 +23,116 @@
     ?>
         <header>
 
-        <nav class="nav-bar">
-            <a href=""><img class="logo-img" src="../img/pai_coruja_branca.png"></a>
-            <ul class="ul-area-btn">
-                <li class="nav-li"><a class="btn-nav-open"><i class="fas fa-bars"></i></a></li>
-            </ul>
-        </nav>
+<nav class="nav-bar">
+    <a href=""><img class="logo-img" src="../img/pai_coruja_branca.png"></a>
+    <ul class="ul-area-btn">
+        <li class="nav-li"><a class="btn-nav-open"><i class="fas fa-bars"></i></a></li>
+    </ul>
+</nav>
 
-        <div class="sidebar">
-            <div class="logo-content">
-                <div class="logo">
-                    <div class="logo-name"><a href="home-secretaria.php"><img src="../img/pai_coruja_branca.png"></a></div>
-                    <div class="close-mobile-navbar">
-                        <span>Menu Pai Coruja</span>
-                        <a class="btn-nav-close"><i class="far fa-window-close"></i></a>
-                    </div>
-                </div>
+<div class="sidebar">
+    <div class="logo-content">
+        <div class="logo">
+            <div class="logo-name"><a href="home-adm.php"><img src="../img/pai_coruja_branca.png"></a>
+            <i class="fas fa-arrow-left"></i>
+        </div>
+            <div class="close-mobile-navbar">
+                <span>Menu Pai Coruja</span>
+                <a class="btn-nav-close"><i class="far fa-window-close"></i></a>
             </div>
-            <ul class="nav-list">
-                <li>
-                    <a onclick="openMenu()" id="sub-menu-button">
-                        <div>
-                            <i class="fas fa-chart-pie"></i>                        
-                        <span class="links-name">Visão Geral</span>
-                        </div>
-                        <i class="fas fa-caret-down" class="dropdown-icon"></i>
-                    </a>
-                </li>
-                <div class="drop-menu" id="sub-menu">
-                    <li class="links-name drop-link">
-                        <a href="home-secretaria.php">
-                            <i class="fas fa-calendar"></i>
-                            <span class="links-name">Mural</span>
-                        </a>
-                    </li>
-                    <li class="links-name drop-link">
-                        <a href="#">
-                            <i class="fas fa-chalkboard-teacher"></i>
-                            <span class="links-name">Avaliação dos Professores</span>
-                        </a>
-                    </li>
-                    <li class="links-name drop-link">
-                        <a href="#">
-                            <i class="fas fa-calendar-day"></i>
-                            <span class="links-name">Eventos Programados</span>
-                        </a>
-                    </li>
-                </div>
-
-                <li>
-                    <a onclick="openMenu2()" id="sub-menu-button-2">
-                        <div>
-                            <i class="fas fa-user-shield"></i>                      
-                        <span class="links-name">Secretaria</span>
-                        </div>
-                        <i class="fas fa-caret-down" class="dropdown-icon"></i>
-                    </a>
-                </li>
-                <div class="drop-menu" id="sub-menu-2">
-                    <li class="links-name drop-link">
-                        <a href="cadastrar-aluno.php">
-                            <i class="fas fa-school"></i>
-                            <span class="links-name">Cadastrar Aluno</span>
-                        </a>
-                    </li>
-                    <li class="links-name drop-link">
-                        <a href="cadastrar-professor.php">
-                            <i class="fas fa-school"></i>
-                            <span class="links-name">Cadastrar Professor</span>
-                        </a>
-                    </li>
-                    <li class="links-name drop-link">
-                        <a href="cadastrar-responsavel.php">
-                            <i class="fas fa-school"></i>
-                            <span class="links-name">Cadastrar Responsável</span>
-                        </a>
-                    </li>
-                    <li class="links-name drop-link">
-                        <a href="cadastrar-turma.php">
-                            <i class="fas fa-school"></i>
-                            <span class="links-name">Cadastrar Turma</span>
-                        </a>
-                    </li>
-                    <li class="links-name drop-link">
-                        <a href="cadastrar-disciplina.php">
-                            <i class="fas fa-school"></i>
-                            <span class="links-name">Cadastrar Disciplina</span>
-                        </a>
-                    </li>
-                    <li class="links-name drop-link">
-                        <a href="cadastrar-horario-turma.php">
-                            <i class="fas fa-school"></i>
-                            <span class="links-name">Cadastrar Horários</span>
-                        </a>
-                    </li>
-                    <li class="links-name drop-link">
-                        <a href="cadastrar-evento.php">
-                            <i class="fas fa-school"></i>
-                            <span class="links-name">Novo Evento</span>
-                        </a>
-                    </li>
-                </div>
-                
-            </ul>
-            <div class="profile-content">
-                <div class="profile">
-                    <div class="profile-details">
-                        <img src="../img/usuario-de-perfil.png" alt="">
-                        <div class="name-job">
-                            <div class="name-menu"><?php echo $_SESSION['nomeSecretaria'] ?></div>
-                            <div class="job-menu">Olá Secretário(a)</div>
-                        </div>
-                    </div>
-                    <div class="profile-logout">
-                        <a href="logout.php">
-                            <i class="fas fa-sign-out-alt" id="logout-user"></i></a>
-                    </div>
+        </div>
+    </div>
+    <ul class="nav-list">
+        <div class="menu-container">
+            <!-- <span>fernfjk</span> -->
+            <li class="links-name">
+                <a href="home-adm.php">
+                    <i class="fas fa-calendar"></i>
+                    <span class="links-name">Mural</span>
+                </a>
+            </li>
+            <li class="links-name">
+                <a href="#">
+                    <i class="fas fa-chalkboard-teacher"></i>
+                    <span class="links-name">Avaliação dos Professores</span>
+                </a>
+            </li>
+            <li class="links-name">
+                <a href="#">
+                    <i class="fas fa-calendar-day"></i>
+                    <span class="links-name">Eventos Programados</span>
+                </a>
+            </li>
+        </div>
+        <hr>
+        <div class="menu-container">
+            <li class="links-name">
+                <a href="cadastrar-turma.php">
+                    <i class="fas fa-school"></i>
+                    <span class="links-name">Cadastrar Turma</span>
+                </a>
+            </li>
+            <li class="links-name">
+                <a href="cadastrar-aluno.php">
+                    <i class="fas fa-school"></i>
+                    <span class="links-name">Cadastrar Aluno</span>
+                </a>
+            </li>
+            <li class="links-name">
+                <a href="cadastrar-professor.php">
+                    <i class="fas fa-school"></i>
+                    <span class="links-name">Cadastrar Professor</span>
+                </a>
+            </li>
+            <li class="links-name">
+                <a href="cadastrar-responsavel.php">
+                    <i class="fas fa-school"></i>
+                    <span class="links-name">Cadastrar Responsável</span>
+                </a>
+            </li>
+            <li class="links-name">
+                <a href="cadastrar-disciplina.php">
+                    <i class="fas fa-school"></i>
+                    <span class="links-name">Cadastrar Disciplina</span>
+                </a>
+            </li>
+            <li class="links-name">
+                <a href="cadastrar-horario-turma.php">
+                    <i class="fas fa-school"></i>
+                    <span class="links-name">Cadastrar Horarios</span>
+                </a>
+            </li>
+            <li class="links-name">
+                <a href="cadastrar-evento.php">
+                    <i class="fas fa-school"></i>
+                    <span class="links-name">Cadastrar Evento</span>
+                </a>
+            </li>
+        </div>
+    </ul>
+    <div class="profile-content">
+        <div class="profile-menu">
+            <a href="logout.php">
+                <i class="fas fa-sign-out-alt" id="logout-user"></i>
+                <span>Logout</span>
+            </a>
+            <a href="#">
+                <i class="fas fa-user-cog"></i>
+                <span>Configurações</span>
+            </a>
+        </div>
+        <div class="profile">
+            <div class="profile-details">
+                <img src="../img/usuario-de-perfil.png" alt="">
+                <div class="name-job">
+                    <div class="name-menu"><?php echo $_SESSION['nomeSecretaria'] ?></div>
+                    <div class="job-menu">Olá Secretário(a)</div>
                 </div>
             </div>
         </div>
-
-        </header>
+    </div>
+</div>
+</header>
 
 
         <main class="container-main">
