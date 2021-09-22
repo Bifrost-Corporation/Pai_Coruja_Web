8e46a5a1c9bd4ffb2a6a67ec98aa10dde0984c01
Douglas Nascimento <douglas.dans@gmail.com>
2021-09-22 18:51:31 -0300

    Adivinha,mais atualiações do menu

--- js/nav.js
+++ js/nav.js
@@ -16,40 +16,24 @@ function Navbar(botaoAbrir, menu, botaoFechar) {
 
 }
 
-const mobileMenu = new Navbar(".btn-nav-open", ".sidebar", ".btn-nav-close")
+const mobileMenu = new Navbar(".btn-nav-open", ".sidebar", ".btn-nav-close", ".container-main")
 
-// DROPDOWN AINDA N FUNFA COM A FUNCTION, MAS VAI FUNFAR
+function Navbar1(botaoAbrir, menu, botaoFechar, container) {
+    const botaoAbrirNav = document.querySelector(botaoAbrir)
+    const menuNav = document.querySelector(menu)
+    const botaoFecharNav = document.querySelector(botaoFechar)
+    const containerMain = document.querySelector(container)
 
-const subMenu = document.getElementById("sub-menu");
-const subMenu2 = document.getElementById("sub-menu-2");
-const subMenuButton = document.getElementById("sub-menu-button");
-const subMenuButton2 = document.getElementById("sub-menu-button-2");
+    botaoAbrirNav.addEventListener("click", function() {
+        console.log(menuNav)
+        botaoAbrirNav.firstElementChild.src = "../img/pai2.png"
+        menuNav.classList.toggle("compact-nav")
+        containerMain.classList.toggle("container-maior")
+        if(botaoAbrirNav.firstElementChild.src == "../img/pai2.png"){
+            botaoAbrirNav.firstElementChild.src = "../img/pai_coruja_branca.png"
+        }
+    });
 
-
-function openMenu() {
-    subMenu.style.transition = "4s";
-    subMenu.style.display = "block";
-    subMenuButton.onclick = function() { fechaMenu() };
-    subMenuButton.classList.add("button-drop-opened")
 }
 
-function openMenu2() {
-    subMenu2.style.transition = "4s";
-    subMenu2.style.display = "block";
-    subMenuButton2.onclick = function() { fechaMenu2() };
-    subMenuButton2.classList.add("button-drop-opened")
-}
-
-function fechaMenu() {
-    subMenu.style.transition = "4s";
-    subMenu.style.display = "none";
-    subMenuButton.onclick = function() { openMenu() };
-    subMenuButton.classList.remove("button-drop-opened")
-}
-
-function fechaMenu2() {
-    subMenu2.style.transition = "4s";
-    subMenu2.style.display = "none";
-    subMenuButton2.onclick = function() { openMenu2() };
-    subMenuButton2.classList.remove("button-drop-opened")
-}
+const mobileMenu3 = new Navbar1(".btn-nav-pc-open", ".sidebar", ".btn-nav-close", ".container-main")
