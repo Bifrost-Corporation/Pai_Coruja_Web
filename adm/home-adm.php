7a84f501a7a015e6a9ffbe7f5ffd4bf9cf211816
Douglas Nascimento <douglas.dans@gmail.com>
2021-09-23 12:55:36 -0300

    Mais uma att do menu kkkkkk

--- adm/home-adm.php
+++ adm/home-adm.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
@@ -7,10 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glider-js@1/glider.min.css">
-
+    <meta name="theme-color" content="#042243">
     <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
-    <meta name="theme-color" content="#26466D" />
-    <title>Home - ADM</title>
+    <title>Home - Administrador</title>
 
 
 </head>
