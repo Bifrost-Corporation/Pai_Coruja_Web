9a2eff9ce4c7122d39a06d0f72c99eba9ff11e66
Douglas Nascimento <douglas.dans@gmail.com>
2021-09-28 15:48:12 -0300

    Acho q já dá pra usar esse dashboard

--- secretaria/home-secretaria.php
+++ secretaria/home-secretaria.php
@@ -18,232 +18,138 @@
 </head>
 
 <body>
-    <?php
-        include ('sentinela.php');
+<?php
+        include("sentinela.php");
+    
     ?>
-        <header>
+    
+    <header>
 
-<nav class="nav-bar">
-    <a href=""><img class="logo-img" src="../img/pai_coruja_branca.png"></a>
-    <ul class="ul-area-btn">
-        <li class="nav-li"><a class="btn-nav-open"><i class="fas fa-bars"></i></a></li>
-    </ul>
-</nav>
+        <nav class="nav-bar">
+            <a href=""><img class="logo-img" src="../img/pai_coruja_branca.png"></a>
+            <ul class="ul-area-btn">
+                <li class="nav-li"><a class="btn-nav-open"><i class="fas fa-bars"></i></a></li>
+            </ul>
+        </nav>
 
-<div class="sidebar">
-    <div class="logo-content">
-        <div class="logo">
-            <div class="logo-name"><a href="home-adm.php"><img src="../img/pai_coruja_branca.png"></a>
-            <i class="fas fa-arrow-left"></i>
-        </div>
-            <div class="close-mobile-navbar">
-                <span>Menu Pai Coruja</span>
-                <a class="btn-nav-close"><i class="far fa-window-close"></i></a>
+        <div class="sidebar">
+            <div class="logo-content">
+                <div class="logo">
+                    <div class="logo-name"><a class="btn-nav-pc-open"><img src="../img/pai_coruja_branca.png"></a>
+                    <i class="fas fa-arrow-left"></i>
+                </div>
+                    <div class="close-mobile-navbar">
+                        <span>Menu Pai Coruja</span>
+                        <a class="btn-nav-close"><i class="far fa-window-close"></i></a>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    <ul class="nav-list">
-        <div class="menu-container">
-            <!-- <span>fernfjk</span> -->
-            <li class="links-name">
-                <a href="home-secretaria.php">
-                    <i class="fas fa-calendar"></i>
-                    <span class="links-name">Mural</span>
-                </a>
-            </li>
-            <li class="links-name">
-                <a href="#">
-                    <i class="fas fa-chalkboard-teacher"></i>
-                    <span class="links-name">Avaliação dos Professores</span>
-                </a>
-            </li>
-            <li class="links-name">
-                <a href="#">
-                    <i class="fas fa-calendar-day"></i>
-                    <span class="links-name">Eventos Programados</span>
-                </a>
-            </li>
-        </div>
-        <hr>
-        <div class="menu-container">
-            <li class="links-name">
-                <a href="cadastrar-turma.php">
-                    <i class="fas fa-school"></i>
-                    <span class="links-name">Cadastrar Turma</span>
-                </a>
-            </li>
-            <li class="links-name">
-                <a href="cadastrar-aluno.php">
-                    <i class="fas fa-school"></i>
-                    <span class="links-name">Cadastrar Aluno</span>
-                </a>
-            </li>
-            <li class="links-name">
-                <a href="cadastrar-professor.php">
-                    <i class="fas fa-school"></i>
-                    <span class="links-name">Cadastrar Professor</span>
-                </a>
-            </li>
-            <li class="links-name">
-                <a href="cadastrar-responsavel.php">
-                    <i class="fas fa-school"></i>
-                    <span class="links-name">Cadastrar Responsável</span>
-                </a>
-            </li>
-            <li class="links-name">
-                <a href="cadastrar-disciplina.php">
-                    <i class="fas fa-school"></i>
-                    <span class="links-name">Cadastrar Disciplina</span>
-                </a>
-            </li>
-            <li class="links-name">
-                <a href="cadastrar-horario-turma.php">
-                    <i class="fas fa-school"></i>
-                    <span class="links-name">Cadastrar Horarios</span>
-                </a>
-            </li>
-            <li class="links-name">
-                <a href="cadastrar-evento.php">
-                    <i class="fas fa-school"></i>
-                    <span class="links-name">Cadastrar Evento</span>
-                </a>
-            </li>
-        </div>
-    </ul>
-    <div class="profile-content">
-        <div class="profile-menu">
-            <a href="logout.php">
-                <i class="fas fa-sign-out-alt" id="logout-user"></i>
-                <span>Logout</span>
-            </a>
-            <a href="#">
-                <i class="fas fa-user-cog"></i>
-                <span>Configurações</span>
-            </a>
-        </div>
-        <div class="profile">
-            <div class="profile-details">
-                <img src="../img/usuario-de-perfil.png" alt="">
-                <div class="name-job">
-                    <div class="name-menu"><?php echo $_SESSION['nomeSecretaria'] ?></div>
-                    <div class="job-menu">Olá Secretário(a)</div>
+            <ul class="nav-list">
+                <div class="menu-container">
+                    <!-- <span>fernfjk</span> -->
+                    <li class="links-name">
+                        <a href="home-adm.php" class="active-nav">
+                            <i class="fas fa-calendar"></i>
+                            <span class="links-name">Mural</span>
+                        </a>
+                    </li>
+                    <li class="links-name">
+                        <a href="#">
+                            <i class="fas fa-chalkboard-teacher"></i>
+                            <span class="links-name">Avaliação dos Professores</span>
+                        </a>
+                    </li>
+                    <li class="links-name">
+                        <a href="#">
+                            <i class="fas fa-calendar-day"></i>
+                            <span class="links-name">Eventos Programados</span>
+                        </a>
+                    </li>
+                </div>
+                <hr>
+                <div class="menu-container">
+                    <li class="links-name">
+                        <a href="cadastrar-escola.php">
+                            <i class="fas fa-school"></i>
+                            <span class="links-name">Cadastrar Escola</span>
+                        </a>
+                    </li>
+                    <li class="links-name">
+                        <a href="cadastrar-secretaria.php">
+                            <i class="fas fa-school"></i>
+                            <span class="links-name">Cadastrar Secretária</span>
+                        </a>
+                    </li>
+                </div>
+            </ul>
+            <div class="profile-content">
+                <div class="profile-menu">
+                    <a href="logout.php">
+                        <i class="fas fa-sign-out-alt" id="logout-user"></i>
+                        <span>Logout</span>
+                    </a>
+                    <a href="#">
+                        <i class="fas fa-user-cog"></i>
+                        <span>Configurações</span>
+                    </a>
+                </div>
+                <div class="profile">
+                    <div class="profile-details">
+                        <img src="../img/usuario-de-perfil.png" alt="">
+                        <div class="name-job">
+                            <div class="name-menu">Admin</div>
+                            <div class="job-menu">Olá Administrador(a)</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-</header>
+    </header>
+        
 
 
-        <main class="container-main">
-        <div class="carousel">
-            <div class=" carousel-destaque">
-                <div class="destaque-card">
-                <div class="esquerda-destaque">
-                    <h1>Titulo</h1>
-                    <small>Subtitulo</small>
-                    <p>Para mais detalhes clique abaixo</p>
-
-                    <a href="#"><button class="saiba-mais-btn">Saiba Mais</button></a>
+    <main class="container-main">
+        <div class="dashboard">
+            <div class="ola-nav-dash">
+                <div>
+                    <h1>Olá Secretário</h1>
+                    <small>Etec De Guaianazes</small>
                 </div>
-                <div class="direita-destaque">
-
-                </div> 
             </div>
-            
-            <div class="destaque-card">
-                <div class="esquerda-destaque">
-                    <h1>Titulo<!--<?php echo $linha['tituloPublicacao'] ?>--></h1>
-                    <small>Subtitulo<!--<?php echo $linha['descPublicacao'] ?>--></small>
-                    <p>Para mais detalhes clique abaixo</p>
-
-                    <a href="#"><button class="saiba-mais-btn">Saiba Mais</button></a>
+            <div class="container-dash">
+                <div class="left-dash">
+                    <h3>Acesso Rápido</h3>
+                    <div class="acesso-rapido-btns">
+                        <a href="#">AAAAAA</a>
+                        <a href="#">AAAAAA</a>
+                    </div>
+                    <div class="dash-eventos-destaque">
+                        <h3>Eventos em destaque</h3>
+                        <div class="evento-card">
+                            <div>
+                                <h1>Evento</h1>
+                                <small>Data: 06/06/2006</small>
+                            </div>
+                            <div>
+                                <a href="#"><button>Saiba Mais</button></a>
+                            </div>
+                        </div>
+                        <div class="evento-card">
+                            <div>
+                                <h1>Evento</h1>
+                                <small>Data: 06/06/2006</small>
+                            </div>
+                            <div>
+                                <a href="#"><button>Saiba Mais</button></a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="direita-destaque">
-
-                </div> 
-            </div>
-            <div class="destaque-card">
-                <div class="esquerda-destaque">
-                    <h1>Titulo<!--<?php echo $linha['tituloPublicacao'] ?>--></h1>
-                    <small>Subtitulo<!--<?php echo $linha['descPublicacao'] ?>--></small>
-                    <p>Para mais detalhes clique abaixo</p>
-
-                    <a href="#"><button class="saiba-mais-btn">Saiba Mais</button></a>
+                <div class="right-dash">
+                    fgdsgdfsgdfgdsg
                 </div>
-                <div class="direita-destaque">
-                    
-                </div> 
-            </div>
-            </div>
-            <div class="indicadors-bar">
-            <i aria-label="Previous" class="fa fa-chevron-left carousel-destaque-prev"></i>
-            <div role="tablist" class="carousel-destaque-dots"></div>
-            <i aria-label="Next" class="fa fa-chevron-right carousel-destaque-next"></i>
-            
             </div>
         </div>
-       
-
-        <section class="cards-eventos">
-            <h2>Eventos</h2>
-            <div class="arrumar-cards carousel-evento">
-
-                <div class="evento-card">
-                    <div>
-                        <h1>Evento</h1>
-                        <small>Data: 06/06/2006</small>
-                    </div>
-                    <div>
-                        <a href="#"><button>Saiba Mais</button></a>
-                    </div>
-                </div>
-
-                <div class="evento-card">
-                    <div>
-                        <h1>Evento</h1>
-                        <small>Data: 06/06/2006</small>
-                    </div>
-                    <div>
-                        <a href="#"><button>Saiba Mais</button></a>
-                    </div>
-                </div>
-                <div class="evento-card">
-                    <div>
-                        <h1>Evento</h1>
-                        <small>Data: 06/06/2006</small>
-                    </div>
-                    <div>
-                        <a href="#"><button>Saiba Mais</button></a>
-                    </div>
-                </div>
-                <div class="evento-card">
-                    <div>
-                        <h1>Evento</h1>
-                        <small>Data: 06/06/2006</small>
-                    </div>
-                    <div>
-                        <a href="#"><button>Saiba Mais</button></a>
-                    </div>
-                </div>
-                <div class="evento-card">
-                    <div>
-                        <h1>Evento</h1>
-                        <small>Data: 06/06/2006</small>
-                    </div>
-                    <div>
-                        <a href="#"><button>Saiba Mais</button></a>
-                    </div>
-                </div>
-
-
-            </div>
-            <div class="indicadors-bar">
-            <i aria-label="Previous" class="fa fa-chevron-left carousel-evento-prev"></i>
-            <div role="tablist" class="carousel-evento-dots"></div>
-            <i aria-label="Next" class="fa fa-chevron-right carousel-evento-next"></i>
-            </div>
-        </section>
     </main>
 
     <div class="nav-footer">
