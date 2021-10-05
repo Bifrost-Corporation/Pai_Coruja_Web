1fb02aee01f3ad5bc2043df179abeaf3cd9d9c74
Douglas Nascimento <douglas.dans@gmail.com>
2021-10-04 23:03:23 -0300

    dashboard tá surgindo

--- secretaria/home-secretaria.php
+++ secretaria/home-secretaria.php
@@ -15,60 +15,56 @@
     <title>Home - Secretária</title>
 
 
-</head>
-
-<body>
-<?php
-        include("sentinela.php");
-    
+    <?php
+        include ('sentinela.php');
+        include ('globalSecretaria.php');
     ?>
-    
-    <header>
+       <header>
 
-        <nav class="nav-bar">
-            <a href=""><img class="logo-img" src="../img/pai_coruja_branca.png"></a>
-            <ul class="ul-area-btn">
-                <li class="nav-li"><a class="btn-nav-open"><i class="fas fa-bars"></i></a></li>
-            </ul>
-        </nav>
+<nav class="nav-bar">
+    <a href=""><img class="logo-img" src="../img/pai_coruja_branca.png"></a>
+    <ul class="ul-area-btn">
+        <li class="nav-li"><a class="btn-nav-open"><i class="fas fa-bars"></i></a></li>
+    </ul>
+</nav>
 
-        <div class="sidebar">
-            <div class="logo-content">
-                <div class="logo">
-                    <div class="logo-name"><a class="btn-nav-pc-open"><img src="../img/pai_coruja_branca.png"></a>
-                    <i class="fas fa-arrow-left"></i>
-                </div>
-                    <div class="close-mobile-navbar">
-                        <span>Menu Pai Coruja</span>
-                        <a class="btn-nav-close"><i class="far fa-window-close"></i></a>
-                    </div>
-                </div>
+<div class="sidebar">
+    <div class="logo-content">
+        <div class="logo">
+            <div class="logo-name"><a href="home-adm.php"><img src="../img/pai_coruja_branca.png"></a>
+            <i class="fas fa-arrow-left"></i>
+        </div>
+            <div class="close-mobile-navbar">
+                <span>Menu Pai Coruja</span>
+                <a class="btn-nav-close"><i class="far fa-window-close"></i></a>
             </div>
-            <ul class="nav-list">
-                <div class="menu-container">
-                    <!-- <span>fernfjk</span> -->
-                    <li class="links-name">
-                        <a href="home-adm.php" class="active-nav">
-                            <i class="fas fa-calendar"></i>
-                            <span class="links-name">Mural</span>
-                        </a>
-                    </li>
-                    <li class="links-name">
-                        <a href="#">
-                            <i class="fas fa-chalkboard-teacher"></i>
-                            <span class="links-name">Avaliação dos Professores</span>
-                        </a>
-                    </li>
-                    <li class="links-name">
-                        <a href="#">
-                            <i class="fas fa-calendar-day"></i>
-                            <span class="links-name">Eventos Programados</span>
-                        </a>
-                    </li>
-                </div>
-                <hr>
-                <div class="menu-container">
-                <li class="links-name">
+        </div>
+    </div>
+    <ul class="nav-list">
+        <div class="menu-container">
+            <!-- <span>fernfjk</span> -->
+            <li class="links-name">
+                <a href="home-secretaria.php">
+                    <i class="fas fa-calendar"></i>
+                    <span class="links-name">Mural</span>
+                </a>
+            </li>
+            <li class="links-name">
+                <a href="#">
+                    <i class="fas fa-chalkboard-teacher"></i>
+                    <span class="links-name">Avaliação dos Professores</span>
+                </a>
+            </li>
+            <li class="links-name">
+                <a href="#">
+                    <i class="fas fa-calendar-day"></i>
+                    <span class="links-name">Eventos Programados</span>
+                </a>
+            </li>
+        </div>
+        <hr>
+        <div class="menu-container">
+            <li class="links-name">
                 <a href="cadastrar-turma.php">
                     <i class="fas fa-school"></i>
                     <span class="links-name">Cadastrar Turma</span>
@@ -110,76 +106,67 @@
                     <span class="links-name">Cadastrar Evento</span>
                 </a>
             </li>
-                </div>
-            </ul>
-            <div class="profile-content">
-                <div class="profile-menu">
-                    <a href="logout.php">
-                        <i class="fas fa-sign-out-alt" id="logout-user"></i>
-                        <span>Logout</span>
-                    </a>
-                    <a href="#">
-                        <i class="fas fa-user-cog"></i>
-                        <span>Configurações</span>
-                    </a>
-                </div>
-                <div class="profile">
-                    <div class="profile-details">
-                        <img src="../img/usuario-de-perfil.png" alt="">
-                        <div class="name-job">
-                            <div class="name-menu">Admin</div>
-                            <div class="job-menu">Olá Administrador(a)</div>
-                        </div>
-                    </div>
+        </div>
+    </ul>
+    <div class="profile-content">
+        <div class="profile-menu">
+            <a href="logout.php">
+                <i class="fas fa-sign-out-alt" id="logout-user"></i>
+                <span>Logout</span>
+            </a>
+            <a href="#">
+                <i class="fas fa-user-cog"></i>
+                <span>Configurações</span>
+            </a>
+        </div>
+        <div class="profile">
+            <div class="profile-details">
+                <img src="../img/usuario-de-perfil.png" alt="">
+                <div class="name-job">
+                    <div class="name-menu"><?php echo $_SESSION['nomeSecretaria'] ?></div>
+                    <div class="job-menu">Olá Secretário(a)</div>
                 </div>
             </div>
         </div>
-    </header>
+    </div>
+</div>
+</header>
+
+        
+    
+
+    <main>
+        
+        
+        
+        <div class="container-dash">
+            <div>
+                <h1>Dashboard</h1>
+            </div>
+
+            <div class="container-dados-dash">
+                <div class="acesso-rapido-dash">
+                    <h4>Acesso Rápido</h4>
+                    <div class="acesso-dash-btns">
+                        <a href=""><button>Cadastrar Dados</button></a>
+                        <a href=""><button>Cadastrar Dados</button></a>
+                        <a href=""><button>Cadastrar Dados</button></a>
+                        <a href=""><button>Cadastrar Dados</button></a>
+                    </div>
+                </div>
+                <div class="acesso-rapido-dash">
+                    <h4>Dados Escolares</h4>
+                </div>
+                <div class="acesso-rapido-dash">
+                    <h4>Mensagens do Chat</h4>
+                </div>
+            </div>
+            
+        </div>
+
         
 
 
-    <main class="container-main">
-        <div class="dashboard">
-            <div class="ola-nav-dash">
-                <div>
-                    <h1>Olá Secretário</h1>
-                    <small>Etec De Guaianazes</small>
-                </div>
-            </div>
-            <div class="container-dash">
-                <div class="left-dash">
-                    <h3>Acesso Rápido</h3>
-                    <div class="acesso-rapido-btns">
-                        <a href="#">AAAAAA</a>
-                        <a href="#">AAAAAA</a>
-                    </div>
-                    <div class="dash-eventos-destaque">
-                        <h3>Eventos em destaque</h3>
-                        <div class="evento-card">
-                            <div>
-                                <h1>Evento</h1>
-                                <small>Data: 06/06/2006</small>
-                            </div>
-                            <div>
-                                <a href="#"><button>Saiba Mais</button></a>
-                            </div>
-                        </div>
-                        <div class="evento-card">
-                            <div>
-                                <h1>Evento</h1>
-                                <small>Data: 06/06/2006</small>
-                            </div>
-                            <div>
-                                <a href="#"><button>Saiba Mais</button></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="right-dash">
-                    fgdsgdfsgdfgdsg
-                </div>
-            </div>
-        </div>
     </main>
 
     <div class="nav-footer">
