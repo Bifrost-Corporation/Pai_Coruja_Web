79d0ede79798815a0e76cb92f3777e3684dab0ca
Douglas Nascimento <douglas.dans@gmail.com>
2021-10-16 19:35:18 -0300

    alteração pequena nas tabelas de alteração
    Co-Authored-By: ma <54721+ma@users.noreply.github.com>

--- secretaria/dashboard.php
+++ secretaria/dashboard.php
@@ -94,7 +94,6 @@
         }
     ?>
        <header>
-
             <nav class="nav-bar">
                 <a href=""><img class="logo-img" src="../img/pai_coruja_branca.png"></a>
                 <ul class="ul-area-btn">
