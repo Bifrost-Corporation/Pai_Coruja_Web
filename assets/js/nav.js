e5da4f90a583fba7954c765a9086deb687521943
Douglas Nascimento <douglas.dans@gmail.com>
2021-11-05 17:30:14 -0300

    sidebar menu compacto praticamente pronto

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -32,20 +32,22 @@ const dropdownProfile = document.querySelector(".dropdown-menu-profile")
 
 // const mobileMenu = new Navbar(".btn-nav-open", ".sidebar", ".btn-nav-close", ".container-main")
 
-function Navbar1(botaoAbrir, menu, botaoFechar, container) {
+function Navbar1(botaoAbrir, menu, botaoFechar, container,container2) {
     const botaoAbrirNav = document.querySelector(botaoAbrir)
     const menuNav = document.querySelector(menu)
     const botaoFecharNav = document.querySelector(botaoFechar)
     const containerMain = document.querySelector(container)
+    const containerDash = document.querySelector(container2)
 
     botaoAbrirNav.addEventListener("click", function() {
         console.log(menuNav)
 
         menuNav.classList.toggle("sidebar-compact")
         containerMain.classList.toggle("container-maior")
+        containerDash.classList.toggle("container-maior")
     });
 
 }
 
-const mobileMenu3 = new Navbar1(".btn-nav-pc-open", ".sidebar", ".btn-nav-pc-open", ".container-main")
+const mobileMenu3 = new Navbar1(".btn-nav-pc-open", ".sidebar", ".btn-nav-pc-open", ".container-main",".container-dash")
 
