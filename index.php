e1ce701d30621ece7ffbedf7adfb32a62ad4c4eb
Douglas Nascimento <douglas.dans@gmail.com>
2021-11-23 23:05:20 -0300

    todas as pag agr em PT-BR + icone da guia adicionado em todas as páginas tbm

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#042243">
+    <link rel="shortcut icon" href="favicon.ico">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     <link rel="manifest" href="manifest.json">
